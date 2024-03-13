4e0fc44581b6ad30d6d77f41b4d05f1c7d30d736
klororf <dchernandez27@outlook.com>
2024-03-13 17:27:08 +0100

    Added number comparers (#1)

--- Utils/OdataOperatorsUtils.cs
+++ Utils/OdataOperatorsUtils.cs
@@ -4,6 +4,11 @@ namespace Klororf.AzSearchLinq.Utils;
 internal static class OdataOperatorsUtils{
     internal static Dictionary<ExpressionType, string>  OdataOperator = new Dictionary<ExpressionType,string>(){
        {ExpressionType.Equal,"eq"},
-       {ExpressionType.NotEqual, "ne"}
+       {ExpressionType.NotEqual, "ne"},
+       {ExpressionType.LessThanOrEqual,"le"},
+       {ExpressionType.LessThan,"lt"},
+       {ExpressionType.GreaterThan,"gt"},
+       {ExpressionType.GreaterThanOrEqual,"ge"}
+       
     };
 }
