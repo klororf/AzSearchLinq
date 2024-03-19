e74469c74722315671c5efa844289c5f69189531
klororf <dchernandez27@outlook.com>
2024-03-19 18:19:21 +0100

    Updated logic for apply logical operators without parentheses

--- Extensions/ExpressionTypeExtensions.cs
+++ Extensions/ExpressionTypeExtensions.cs
@@ -7,6 +7,14 @@ public static class ExpressionTypeExtensions{
         return expressionType switch
         {
             ExpressionType.Equal or ExpressionType.NotEqual or ExpressionType.GreaterThanOrEqual or ExpressionType.GreaterThan or ExpressionType.LessThanOrEqual or ExpressionType.LessThan => true,
+           
+            _ => false,
+        };
+    }
+        public static bool IsLogicalOperator (this ExpressionType expressionType){
+        return expressionType switch
+        {
+           ExpressionType.And or ExpressionType.AndAlso or ExpressionType.Or or ExpressionType.OrElse => true,
             _ => false,
         };
     }
